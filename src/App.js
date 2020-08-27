c95e4d5af453d94b37db000cc140c320b67cc96e
casanova <casanovaxie@magicengine.com.cn>
2020-08-27 18:22:26 +0800

    添加类型比较

--- src/App.js
+++ src/App.js
@@ -30,8 +30,17 @@ class Test extends Component {
         <div style={{ width: 100, backgroundColor: "red", height: "100vh" }}>
           {testa}
         </div>
-        <div style={{ flex: 2, backgroundColor: "green", height: "100vh" }}>
+        <div style={{ flex: 1, backgroundColor: "green", height: "100vh" }}>
           {this.context}
+        </div>
+        <div style={{ flex: 1 }}>
+          <h1>两种类型相加，如果一边是字符串，那么都转成字符串相加</h1>
+          <h1>两种类型相加，如果没有字符串，那么都转成数字相加</h1>
+        </div>
+        <div style={{ flex: 1 }}>
+          <h1>1.两种类型比较,类型相同,比较值</h1>
+          <h1>2.(基础类型)类型不同,转数字比较值</h1>
+          <h1>3.(引用类型)调用valueOf然后调用toString转成基础类型,然后从回到第一步的流程</h1>
         </div>
       </div>
     );
