53b6c8a3bd3633fff10e22913d81c716f6f806f0
casanova <casanovaxie@magicengine.com.cn>
2020-09-07 10:59:15 +0800

    复习call,bind,new,instanceof,添加数字转化成2进制

---
+++ src/ES/toString.js
@@ -0,0 +1,5 @@
+const num = 6;
+
+const num_to_two = num.toString(2);
+const num_to_eight = num.toString(8);
+const num_to_sixteen = num.toString(16);
--- src/test.js
+++ src/test.js
@@ -1,252 +1,54 @@
-const bubbleSort = (array) => {
-  for (let i = 0; i < array.length; i++) {
-    let flag = true;
-    for (let j = 0; j < array.length - 1 - i; j++) {
-      if (array[j] > array[j + 1]) {
-        [array[j], array[j + 1]] = [array[j + 1], [array[j]]];
-        flag = false;
-      }
-    }
-    if (flag) {
-      break;
-    }
+Function.prototype.myCall = function (obj, ...args) {
+  if (typeof obj !== "object") {
+    throw new Error("xxx");
   }
-  return array;
-};
-
-const insertSort = (array) => {
-  for (let i = 0; i < array.length; i++) {
-    let target = i;
-    for (let j = i - 1; j >= 0; j--) {
-      if (array[target] < array[j]) {
-        [array[j], array[target]] = [array[target], array[j]];
-        target = j;
-      } else {
-        break;
-      }
-    }
-  }
-  return array;
-};
-
-const quickSort = (array) => {
-  if (array.length < 2) {
-    return array;
-  }
-  let target = array[0];
-  const left = [];
-  const right = [];
-  for (let i = 1; i < array.length; i++) {
-    if (array[i] < target) {
-      left.push(array[i]);
-    } else {
-      right.push(array[i]);
-    }
-  }
-  return [...quickSort(left), target, ...quickSort(right)];
-};
-
-const merge = (left, right) => {
-  const res = [];
-  while (left.length && right.length) {
-    if (left[0] > right[0]) {
-      res.push(right.shift());
-    } else {
-      res.push(left.shift());
-    }
-  }
-
-  while (left.length) {
-    res.push(left.shift());
-  }
-  while (right.length) {
-    res.push(right.shift());
-  }
+  const fn = Symbol();
+  obj[fn] = this;
+  const res = obj[fn](...args);
+  delete obj[fn];
   return res;
 };
 
-const mergeSort = (array) => {
-  if (array.length < 2) {
-    return array;
+Function.prototype.myBind = function (obj, ...args) {
+  if (typeof obj !== "object") {
+    throw new Error("xxx");
   }
-  let index = Math.floor(array.length / 2);
-  let left = array.slice(0, index);
-  let right = array.slice(index);
-  return merge(mergeSort(left), mergeSort(right));
-};
-
-const selectSort = (array) => {
-  for (let i = 0; i < array.length; i++) {
-    let min_index = i;
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[min_index > array[j]]) {
-        min_index = j;
-      }
-    }
-    [array[min_index], array[i]] = [array[i], array[min_index]];
-  }
-  return array;
-};
-
-const debounce = (fn, delay) => {
-  let timer = null;
+  const _this = this;
   return function () {
-    const args = [].slice.apply(arguments);
-    timer = setTimeout(() => {
-      timer && clearTimeout(timer);
-      fn(...args);
-    }, delay);
+    _this.call(obj, ...args);
   };
 };
 
-const throttle = (fn, delay) => {
-  let timer = null;
-  return function () {
-    const args = [].slice.apply(arguments);
-    if (!timer) {
-      timer = setTimeout(() => {
-        fn(...args);
-        clearTimeout(timer);
-        timer = null;
-      }, delay);
-    }
-  };
-};
-
-function MyPromise(executor) {
-  this.status = "PENDING";
-  this.value = null;
-  this.reason = null;
-
-  this.resolves = [];
-  this.rejects = [];
-
-  this.resolve = (value) => {
-    if (this.status === "PENDING") {
-      this.status = "RESOLVED";
-      this.value = value;
-    }
-    while (resolves.length) {
-      const tem = resolves.shift();
-      tem(value);
-    }
-  };
-
-  this.reject = (reason) => {
-    if (this.status === "PENDING") {
-      this.status = "REJECTED";
-      this.reason = reason;
-    }
-    while (rejects.length) {
-      const tem = rejects.shift();
-      tem(value);
-    }
-  };
-
-  this.then = (resolve, reject) => {
-    resolve = typeof resolve === "function" ? resolve : (v) => v;
-    reject =
-      typeof reject === "function"
-        ? reject
-        : (e) => {
-            throw new Error(e);
-          };
-
-    return MyPromise((resolveFn, rejectFn) => {
-      const fulfilled = (value) => {
-        try {
-          const res = resolve(value);
-          res instanceof MyPromise
-            ? MyPromise.then(resolveFn, rejectFn)
-            : resolveFn(res);
-        } catch (error) {
-          reject(error);
-        }
-      };
-
-      const rejected = (reason) => {
-        try {
-          const res = reject(reason);
-          res instanceof MyPromise
-            ? MyPromise.then(resolveFn, rejectFn)
-            : rejectFn(res);
-        } catch (error) {
-          reject(error);
-        }
-      };
-
-      switch (this.status) {
-        case "RESOLVED":
-          fulfilled(this.value);
-          break;
-        case "REJECTED":
-          rejected(this.reason);
-          break;
-        case "PENDING":
-        default:
-          this.resolves.push(fulfilled);
-          this.rejects.push(rejected);
-          break;
-      }
-    });
-  };
-
-  try {
-    executor(this.resolve, this.reject);
-  } catch (error) {
-    this.reject(error);
+Function.prototype.myNew = function (Construe) {
+  if (typeof Construe !== "object") {
+    throw new Error("xxx");
   }
-}
-
-const greedy = (g, s) => {
-  g = g.sort((a, b) => b - a);
-  s = s.sort((a, b) => b - a);
-  let cookie = 0;
-  let child = 0;
-  let num = 0;
-  while (cookie < g.length && child < s.length) {
-    if (g[cookie] >= s[child]) {
-      child++;
-      num++;
-    }
-    cookie++;
-  }
-  return num;
-};
-
-var cuttingRope = function (n) {
-  if (n === 2) return 1;
-  if (n === 3) return 2;
-  let a = ~~(n / 3);
-  let b = n % 3;
-  if (b === 0) return Math.pow(3, a);
-  if (b === 1) return Math.pow(3, a - 1) * 4;
-  if (b === 2) return Math.pow(3, a) * 2;
-};
-
-const findContinuousSequence = (target) => {
-  let l = 1;
-  let r = 1;
-  let res = [];
-  let sum = 0;
-  while (l < target / 2) {
-    if (sum < target) {
-      sum += r;
-      r++;
-    }
-    if (sum > target) {
-      sum -= l;
-      l++;
-    }
-    if (sum === target) {
-      let tem = [];
-      for (let i = l; l < r; i++) {
-        tem.push(i);
-      }
-      res.push(tem);
-      sum -= l;
-      l++;
-    }
+  const obj = Object.create(Construe.prototype);
+  const args = [].slice.call(arguments, 1);
+  const res = Construe.apply(obj, args);
+  const isObject = typeof res === "object" && res !== null;
+  const isFn = typeof res === "function";
+  if (isObject || isFn) {
     return res;
+  }
+  return obj;
+};
+
+const myInstanceOf = (obj, Construe) => {
+  const baseType = ["string", "number", "boolean", "undefined", "symbol"];
+  if (baseType.includes(typeof obj)) {
+    return false;
+  }
+
+  let _pro_ = Object.getPrototypeOf(obj);
+  let pro = Construe.prototype;
+  while (true) {
+    if (_pro_ === null) {
+      return false;
+    }
+    if (_pro_ === pro) {
+      return true;
+    }
+    _pro_ = Object.getPrototypeOf(_pro_);
   }
 };
