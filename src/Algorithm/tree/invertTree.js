fb894bc4651ae58993e1049a4192b4e999b86abc
casanova <casanovaxie@magicengine.com.cn>
2020-09-16 10:26:11 +0800

    ✨ 翻转二叉树问题

---
+++ src/Algorithm/tree/invertTree.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  if (!root) return null;
+  const tem = root.left;
+  root.left = root.right;
+  root.right = tem;
+  invertTree(root.left);
+  invertTree(root.right);
+  return root;
+};
+
+export default invertTree;
