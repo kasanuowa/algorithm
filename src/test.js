c76192ccf658662c0afae974ea8abaa14ec53917
xiejiaqi <2671235470@qq.com>
2020-09-20 10:57:12 +0800

    🤔 复习排序和set结构相关题目

--- src/test.js
+++ src/test.js
@@ -24,3 +24,92 @@ const throttle = (fn, delay) => {
     }
   };
 };
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let flag = true;
+    for (let j = 0; j < array.length - 1 - i; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+        flag = false;
+      }
+    }
+    if (flag) {
+      break;
+    }
+  }
+  return array;
+};
+
+const quickSort = (array) => {
+  if (array.length < 2) {
+    return array;
+  }
+  let index = array[0];
+  let left = [];
+  let right = [];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > index) {
+      right.push(array[i]);
+    } else {
+      left.push(array[i]);
+    }
+  }
+  return [...quickSort(left), index, ...quickSort(right)];
+};
+
+const insertSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let target = i;
+    for (let j = i - 1; j >= 0; j--) {
+      if (array[j] < array[target]) {
+        [array[target], array[j]] = [array[j], array[target]];
+        target = j;
+      } else {
+        break;
+      }
+    }
+  }
+  return array;
+};
+
+const selectSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let min_index = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[min_index] > array[j]) {
+        min_index = j;
+      }
+    }
+    [array[min_index], array[j]] = [array[j], array[min_index]];
+  }
+  return array;
+};
+
+const merge = (left, right) => {
+  const res = [];
+  while (left.length && right.length) {
+    if (left[0] > right[0]) {
+      res.push(right.shift());
+    } else {
+      res.push(left.shift());
+    }
+  }
+  while (left.length) {
+    res.push(left.shift());
+  }
+  while (right.length) {
+    res.push(right.shift());
+  }
+  return res;
+};
+
+const mergeSort = (array) => {
+  if (array.length < 2) {
+    return array;
+  }
+  let index = Math.floor(array.length / 2);
+  let left = array.slice(0, index);
+  let right = array.slice(index);
+  return mergeSort(mergeSort(left), mergeSort(right));
+};
