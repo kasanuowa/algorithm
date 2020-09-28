5f2159c159d90036bd1778aa5de625c29992aac7
casanova <casanovaxie@magicengine.com.cn>
2020-09-28 16:17:18 +0800

    🤔 复习手写promise

--- src/test.js
+++ src/test.js
@@ -150,3 +150,89 @@ function myNew(Construe) {
     return obj;
   }
 }
+
+function MyPromise(executor) {
+  this.status = "PENDING";
+  this.value = null;
+  this.reason = null;
+
+  this.resolves = [];
+  this.rejects = [];
+
+  this.resolve = (value) => {
+    if (this.status === "PENDING") {
+      this.status = "RESOLVED";
+      this.value = value;
+    }
+    while (this.resolves.length) {
+      const tem = this.resolves.shift();
+      tem(value);
+    }
+  };
+
+  this.reject = (reason) => {
+    if (this.status === "PENDING") {
+      this.status = "REJECTED";
+      this.reason = reason;
+    }
+
+    while (this.rejects.length) {
+      const tem = this.rejects.shift();
+      tem(reason);
+    }
+  };
+
+  this.then = (resolve, reject) => {
+    const resolve = typeof resolve === "function" ? resolve : (value) => value;
+    const reject =
+      typeof reject === "function"
+        ? reject
+        : (reason) => {
+            throw new error(reason);
+          };
+
+    return new MyPromise((resolveFn, rejectFn) => {
+      const fulfilled = (value) => {
+        try {
+          const res = resolve(value);
+          res instanceof MyPromise
+            ? MyPromise.then(resolveFn, rejectFn)
+            : resolveFn(res);
+        } catch (error) {
+          reject(error);
+        }
+      };
+
+      const rejected = (reason) => {
+        try {
+          const res = resolve(value);
+          res instanceof MyPromise
+            ? MyPromise.then(resolveFn, rejectFn)
+            : rejectFn(res);
+        } catch (error) {
+          reject(error);
+        }
+      };
+
+      switch (this.status) {
+        case "RESOLVED":
+          fulfilled(this.value);
+          break;
+        case "REJECTED":
+          rejected(this.reason);
+          break;
+        case "PENDING":
+        default:
+          this.resolves.push(fulfilled);
+          this.rejects.push(rejected);
+          break;
+      }
+    });
+  };
+
+  try {
+    executor(this.resolve, this.reject);
+  } catch (error) {
+    this.reject(error);
+  }
+}
