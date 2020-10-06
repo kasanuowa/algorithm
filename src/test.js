ee879ac48598825b04841226fb9406253a8726f5
xiejiaqi <2671235470@qq.com>
2020-10-06 11:14:03 +0800

    🤔 复习斐波那契数列

--- src/test.js
+++ src/test.js
@@ -263,3 +263,13 @@ const findContinuousSequence = (n) => {
   }
   return res;
 };
+
+const fib = (n) => {
+  let arr = Array(n).fill(0);
+  arr[0] = 1;
+  arr[1] = 1;
+  for (let i = 1; i <= n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr[n];
+};
