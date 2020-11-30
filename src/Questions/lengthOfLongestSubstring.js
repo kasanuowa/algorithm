9fa7add0c772510823054bdb321b5b3e2320d624
casanova <casanovaxie@magicengine.com.cn>
2020-11-30 10:43:45 +0800

    ✨ 添加无重复字符的最长字串问题

---
+++ src/Questions/lengthOfLongestSubstring.js
@@ -0,0 +1,19 @@
+/**
+ * 3. 无重复字符的最长子串
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  let map = new Map();
+  let max = 0;
+  for (let i = 0, j = 0; j < s.length; j++) {
+    if (map.has(s[j])) {
+      i = Math.max(map.get(s[j]) + 1, i);
+    }
+    max = Math.max(max, j - i + 1);
+    map.set(s[j], j);
+  }
+  return max;
+};
+
+export default lengthOfLongestSubstring;
